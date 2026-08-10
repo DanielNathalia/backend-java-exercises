fc7f57e1543ca6a4565cebe074c3fc2a9be36059
Natha <nathaliadaniel96@gmail.com>
2026-08-10 15:45:15 -0300

    feat: corrections TabelaDeNumeros

--- semana-3/src/java/TabelaDeNumeros.java
+++ semana-3/src/java/TabelaDeNumeros.java
@@ -1,7 +1,5 @@
 public class TabelaDeNumeros {
     public static void main(String[] args) {
-        // TODO: Imprima números de 1 a 10 (crescente)
-        // TODO: Imprima números de 10 a 1 (decrescente)
         System.out.println("Crescente: ");
         for (int i = 1; i <= 10; i++){
             System.out.println(i + " ");
