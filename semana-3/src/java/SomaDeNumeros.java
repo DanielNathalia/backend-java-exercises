6df05153f5046d9037dce4960902c9c93479a664
Natha <nathaliadaniel96@gmail.com>
2026-08-12 20:11:19 -0300

    feat: Add SomaDeNumeros and Tabuada foundation

---
+++ semana-3/src/java/SomaDeNumeros.java
@@ -0,0 +1,10 @@
+public class SomaDeNumeros {
+    public static void main(String[] args){
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++){
+            soma += i;
+        }
+        System.out.println("Soma de 1 a 100: " + soma);
+    }
+}
